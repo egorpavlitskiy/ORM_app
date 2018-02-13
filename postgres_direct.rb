4fee0c750ed2fba307f4dc20144a3e6105d218ce
Pavlitskiy Egor <e.pavlitskiy@dunice.net>
2018-02-13 23:52:33 +0300

    commit project

---
+++ postgres_direct.rb
@@ -0,0 +1,47 @@
+require 'pg'
+
+class Postgres_direct
+
+  def connect_db
+    @connect = PG.connect :dbname => 'postdb', :user => 'postuser', :password => '12345'
+  end
+
+  def select_all
+    connect_db.exec("SELECT * FROM notes") do |result|
+
+      result.each { |e|
+        id = e["id"]
+        title = e["title"]
+        description = e["description"]
+
+        p "#{id} #{title} #{description}"
+      }
+    end
+    @connect.close
+  end
+
+  def add_entry (params)
+    connect_db.exec("INSERT INTO notes (title, description) values ('#{params[:title]}', '#{params[:description]}')")
+    @connect.close
+  end
+
+  def delete_entry(id)
+    connect_db.exec("DELETE FROM notes WHERE id=#{id}")
+    @connect.close
+  end
+
+  def update_entry(params)
+    connect_db.exec("UPDATE notes SET title='#{params[:title]}', description='#{params[:description]}' WHERE id=#{params[:id]} ")
+    @connect.close
+  end
+
+end
+
+param = {:title => 'Poo' , :description => 'Little story for child'}
+param_update = {:id => 7, :title => 'Poo' , :description => 'Little story for child'}
+
+app = Postgres_direct.new
+# app.add_entry(param)
+app.select_all
+# app.delete_entry(5)
+# app.update_entry(param_update)
